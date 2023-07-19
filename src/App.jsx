0aa9113cb44b85730d6e65d1ac4ba64147853761
Erhan Ertem <eertem@gmail.com>
2023-07-19 17:04:40 +0300

    Provide folder structure and establish page routes (new syntax) via react-route

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './ui/Home'
+import Menu from './features/menu/Menu'
+import Cart from './features/cart/Cart'
+import CreateOrder from './features/order/CreateOrder'
+import Order from './features/order/Order'
+
+//VERY IMPORTANT! THIS NEW WAY OF ROUTING IS REQUIRED TO ENABLE DATA FETCHING W/REACT ROUTER
+const router = createBrowserRouter([
+	{ path: '/', element: <Home /> },
+	{
+		path: '/menu',
+		element: <Menu />,
+	},
+	{
+		path: '/cart',
+		element: <Cart />,
+	},
+	{ path: '/order/new', element: <CreateOrder /> },
+	{ path: '/order/:orderId', element: <Order /> },
+])
+
+function App() {
+	return <RouterProvider router={router} />
+}
+
+export default App
---
+++ src/features/cart/CartItem.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+import formatCurrency from '../../utils/helpers'
+
+function CartItem({ item }) {
+	const { pizzaId, name, quantity, totalPrice } = item
+
+	return (
+		<li>
+			<p>
+				{quantity}&times; {name}
+			</p>
+			<div>
+				<p>{formatCurrency(totalPrice)}</p>
+			</div>
+		</li>
+	)
+}
+
+export default CartItem
