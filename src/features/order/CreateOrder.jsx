2e4cccc2d86ebef80f7e9f7681c23829a62f55cb
Erhan Ertem <eertem@gmail.com>
2023-07-20 23:14:11 +0300

    Error handling via actions

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -2,7 +2,7 @@
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable no-unused-vars */
 import { useState } from 'react'
-import { Form, redirect, useNavigation } from 'react-router-dom'
+import { Form, redirect, useActionData, useNavigation } from 'react-router-dom'
 import { createOrder } from '../../services/apiRestaurant'
 
 // https://uibakery.io/regex-library/phone-number
@@ -40,6 +40,8 @@ function CreateOrder() {
 	// console.log(navigation)
 	const isSubmitting = navigation.state === 'submitting'
 
+	const formErrors = useActionData() //THIS HOOK READS THE DATA RETURNED TO ACTION
+
 	// const [withPriority, setWithPriority] = useState(false);
 	const cart = fakeCart
 
@@ -62,6 +64,7 @@ function CreateOrder() {
 					<div>
 						<input type="tel" name="phone" required />
 					</div>
+					{formErrors?.phone && <p>{formErrors.phone}</p>}
 				</div>
 
 				<div>
@@ -99,7 +102,7 @@ export async function action({ request }) {
 	const formData = await request.formData() //formData() is a regular WEB API @ https://developer.mozilla.org/en-US/docs/Web/API/FormData
 	// console.log(formData)
 	const data = Object.fromEntries(formData) //Convert form data to a readable object
-	console.log('⛔', data)
+	// console.log('⛔', data)
 
 	//RESTRUCTURE THE FORMDATA COMPOSITION
 	const order = {
@@ -109,7 +112,14 @@ export async function action({ request }) {
 	}
 	// console.log(order)
 
-	//EXECUTE FETCHING FUNCTION
+	//ERROR HANDLING FOR INPUTS
+	const errors = {}
+	if (!isValidPhone(order.phone))
+		errors.phone =
+			'Please give us your correct phone number. We might need it to contact you.'
+	if (Object.keys(errors).length) return errors //IF VALID ERROR IS RETURNED TO ACTION AND DOES NOT PROCEED WOITH CREATE ORDER
+
+	//EXECUTE FETCHING FUNCTION IF EVERY THING PASSES W/NO ERROR
 	const newOrder = await createOrder(order) //Returns data so we have to await
 
 	//REFLECT THE ORDERID ON THE URL
