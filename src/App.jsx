f4c6add63fe1fde9247d8d56f42acf45e7291c8d
Erhan Ertem <eertem@gmail.com>
2023-07-20 14:11:03 +0300

    Error handling @ routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,37 @@ import Cart from './features/cart/Cart'
 import CreateOrder from './features/order/CreateOrder'
 import Order from './features/order/Order'
 import AppLayout from './ui/AppLayout'
+import Error from './ui/Error'
 
 //VERY IMPORTANT! THIS NEW WAY OF ROUTING IS REQUIRED TO ENABLE DATA FETCHING W/REACT ROUTER
 const router = createBrowserRouter([
 	{
-		element: <AppLayout />,
+		element: <AppLayout />, //designate the parent element component
+		errorElement: <Error />, //designate the error handler component for incorrect routes
 		children: [
+			//designate the child routes and the corresponding components
 			{ path: '/', element: <Home /> },
 			{
 				path: '/menu',
 				element: <Menu />,
 				loader: menuLoader, //render-as-you-fetch approach from modern react-router
+				errorElement: <Error />, //designate the error handler component - error @ each route bubbles up to parent unless specified as here...
 			},
 			{
 				path: '/cart',
 				element: <Cart />,
+				//error bubbles up to parenting component
 			},
-			{ path: '/order/new', element: <CreateOrder /> },
-			{ path: '/order/:orderId', element: <Order /> },
+			{
+				path: '/order/new',
+				element: <CreateOrder />,
+				//error bubbles up to parenting component
+			},
+			{
+				path: '/order/:orderId',
+				element: <Order />,
+				//error bubbles up to parenting component
+			},
 		],
 	},
 ])
--- src/ui/Error.jsx
+++ src/ui/Error.jsx
@@ -1,15 +1,17 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useRouteError } from 'react-router-dom'
 
-function NotFound() {
-  const navigate = useNavigate();
-
-  return (
-    <div>
-      <h1>Something went wrong 😢</h1>
-      <p>%MESSAGE%</p>
-      <button onClick={() => navigate(-1)}>&larr; Go back</button>
-    </div>
-  );
+function Error() {
+	const navigate = useNavigate()
+	const error = useRouteError()
+	console.log(error)
+	return (
+		<div>
+			<h1>Something went wrong 😢</h1>
+			<p>{error.data || error.message}</p>
+			{/* error from the incorrect errorElement @ Applayout || error from fetching induced problem such as wrong api adress or nonresponsive server */}
+			<button onClick={() => navigate(-1)}>&larr; Go back</button>
+		</div>
+	)
 }
 
-export default NotFound;
+export default Error
