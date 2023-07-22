239754238cc46049a61b8971b5e8de4e90306df5
Erhan Ertem <eertem@gmail.com>
2023-07-23 00:10:38 +0300

    Add selective styling to buttons based on their use case

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,19 +1,19 @@
 /* eslint-disable react-refresh/only-export-components */
-import { useLoaderData } from 'react-router-dom'
-import { getMenu } from '../../services/apiRestaurant'
-import MenuItem from './MenuItem'
+import { useLoaderData } from 'react-router-dom';
+import { getMenu } from '../../services/apiRestaurant';
+import MenuItem from './MenuItem';
 
 function Menu() {
-	//RENDER-AS-YOU-FETCH STRATEGY
-	const menu = useLoaderData() //refers to loader key value @ browserRouter /menu path
-	// console.log(menu)
-	return (
-		<ul>
-			{menu.map(pizza => (
-				<MenuItem pizza={pizza} key={pizza.id} />
-			))}
-		</ul>
-	)
+  //RENDER-AS-YOU-FETCH STRATEGY
+  const menu = useLoaderData(); //refers to loader key value @ browserRouter /menu path
+  // console.log(menu)
+  return (
+    <ul className="divide-y divide-stone-200 px-2">
+      {menu.map((pizza) => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
+  );
 }
 
 /*
@@ -22,8 +22,8 @@ NOTES
 #2 Its a convention to cal it loader()
 */
 export async function loader() {
-	const menu = await getMenu()
-	return menu
+  const menu = await getMenu();
+  return menu;
 }
 
-export default Menu
+export default Menu;
