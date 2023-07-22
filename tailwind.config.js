5156e4532081a25d830290950f96254fd49367a7
Erhan Ertem <eertem@gmail.com>
2023-07-22 23:36:25 +0300

    Tailwind customization - overriding & extending default settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,17 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     fontFamily: {
-      pizza: 'Roboto Mono, monospace',
+      // Whatever put here overrides the entire tailwind default setup
+      sans: 'Roboto Mono, monospace',
     },
-    extend: {},
+    extend: {
+      // Whatever put here adds on top of the default tailwind setup
+      colors: { pizza: '#123456' },
+      // The full list of feature keys available @ https://github.com/tailwindlabs/tailwindcss/blob/master/stubs/config.full.js
+      height: {
+        screen: '100dvh',
+      },
+    },
   },
   plugins: [],
 };
