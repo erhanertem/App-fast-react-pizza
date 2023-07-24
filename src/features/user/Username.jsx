5cf514d880d435719cef952beff6600a0032c945
Erhan Ertem <eertem@gmail.com>
2023-07-24 14:15:29 +0300

    Establish RTK store/slicer

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,12 @@
+import { useSelector } from 'react-redux';
+
 function Username() {
-  return <div className="hidden text-sm font-semibold md:block">ERHAN</div>;
+  const username = useSelector((state) => state.user.username);
+  console.log(username);
+
+  return (
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
+  );
 }
 
 export default Username;
--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,9 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+/*
 function getPosition() {
   return new Promise(function (resolve, reject) {
     navigator.geolocation.getCurrentPosition(resolve, reject);
   });
 }
-
 async function fetchAddress() {
   // 1) We get the user's geolocation position
   const positionObj = await getPosition();
@@ -19,3 +21,22 @@ async function fetchAddress() {
   // 3) Then we return an object with the data that we are interested in
   return { position, address };
 }
+*/
+
+const initialState = {
+  username: 'JONAAAA',
+};
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    updateName(state, action) {
+      state.username = action.payload;
+    },
+  },
+});
+
+export const { updateName } = userSlice.actions;
+
+export default userSlice.reducer;
