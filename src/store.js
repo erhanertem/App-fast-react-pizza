4c4300bbc445888575ca68fdbe843d26ce3d3eab
Erhan Ertem <eertem@gmail.com>
2023-07-25 14:24:12 +0300

    Add cart slicer to the rtk store

--- src/store.js
+++ src/store.js
@@ -2,12 +2,15 @@ import { configureStore } from '@reduxjs/toolkit';
 
 //IMPORT REDUCER SLICE
 import userReducer from './features/user/userSlice';
+import cartReducer from './features/cart/cartSlice';
 
 //CREATE STORE
 const store = configureStore({
   reducer: {
     //REDUCER SLICE
     user: userReducer,
+    //REDUCER SLICE
+    cart: cartReducer,
   },
 });
 
