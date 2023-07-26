6157e4689e47daf855f5a18aa8040ff33e56bd86
Erhan Ertem <eertem@gmail.com>
2023-07-26 16:00:45 +0300

    Implement checkout summary

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -5,36 +5,15 @@ import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../ui/Button';
 import { useSelector } from 'react-redux';
+import { getCart } from '../cart/cartSlice';
+import EmptyCart from '../cart/EmptyCart';
+import { getUsername } from '../user/userSlice';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
     str,
   );
-
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: 'Mediterranean',
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: 'Vegetale',
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: 'Spinach and Mushroom',
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
 
 function CreateOrder() {
   const navigation = useNavigation();
@@ -44,9 +23,11 @@ function CreateOrder() {
   const formErrors = useActionData(); //THIS HOOK READS THE DATA RETURNED TO ACTION
 
   // const [withPriority, setWithPriority] = useState(false);
-  const cart = fakeCart;
-  const username = useSelector((state) => state.user.username);
+  const cart = useSelector(getCart);
+  const username = useSelector(getUsername);
 
+  if (!cart.length) return <EmptyCart />;
+  console.log(cart);
   return (
     <div className="px-4 py-6">
       <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
