49c5538cf4918f1b0455bd3fc264f1629acf769c
Erhan Ertem <eertem@gmail.com>
2024-09-19 19:16:00 +0300

    Display loader indicator in the main app layout area based on RR useNavigator hook's state change

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,10 +1,18 @@
 import Header from './Header';
 import CartOverview from './../features/cart/CartOverview';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
+import Loader from './Loader';
 
 function AppLayout() {
+  const navigation = useNavigation();
+  // console.log(navigation);
+  // NAVIGATIUON STATE IS EITHER IDLE|LOADING
+  const isLoading = navigation.state === 'loading';
+
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
+
       <Header />
 
       <main>
