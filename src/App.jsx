4c24d99f9b2ceb4e2287a225ddec94c803bd7434
Erhan Ertem <eertem@gmail.com>
2024-09-19 19:29:46 +0300

    Handling routing errors @ the parenting element + handling fetch error inside its own route without letting it to bubble to parent element error handling

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Cart from './features/cart/Cart';
 import Order from './features/order/Order';
 import CreateOrder from './features/order/CreateOrder';
 import AppLayout from './ui/AppLayout';
+import Error from './ui/Error';
 
 // #1. CREATE LIST OF FRONT-END ENDPOINTS AND SPA PAGE JSX COMPONENTS
 // NOTE: This is imperative way of declaring routes comapred to traditional route declaration inside JSX via router components. Here all routes are declared outside the JSX body and listed inside an array object. This is an imperative setup to enable data loading , or data fetching with react-router library.
@@ -13,6 +14,8 @@ const router = createBrowserRouter([
   {
     // NOTE: AppLayout without path endpoint definition is a wrapper page - therefore, requires <Outlet/> react-router component to firnish corresponding component in a child route.
     element: <AppLayout />,
+    // WE SPECIFY THE ERRORELEMENT @ THE TOP OF ALL FIELDS AS ERROR WOULD BUBBLES UP FROM THE CHILD ROUTES
+    errorElement: <Error />,
     // Define nested routes here
     children: [
       {
@@ -22,6 +25,8 @@ const router = createBrowserRouter([
       {
         path: '/menu',
         element: <Menu />,
+        // NOTE: Since fetching occurs in this child route, we may encounter error. Instead of destroying the entire layout, we may keep current UI and specify the error message here by specifying the errorElement field in place
+        errorElement: <Error />,
         // NOTE: Provide a loader middleware function to fetch menu data - The fetch function is kept @ Menu component for tidying purposes, however the menuLoader(loader function isnide Menu component) is fired here - The data provided by the menuLoader now could be consuemd within Menu component via useLoaderData RR hook
         loader: menuLoader,
       },
