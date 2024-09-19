543f27d8228df1d9b2db4a9c20886cf9a7f130b8
Erhan Ertem <eertem@gmail.com>
2024-09-19 19:44:59 +0300

    Add order search bar and RR navigate upon submission

---
+++ src/features/order/SearchOrder.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function SearchOrder() {
+  const [query, setQuery] = useState('sffefr');
+
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    // GUARD CLAUSE - no entry nothing happens
+    if (query.trim() === '') return;
+
+    navigate(`/order/${query}`); //Navigate to the entered order#
+    setQuery(''); // Clear the input field after submission
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="Search order #"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+    </form>
+  );
+}
+
+export default SearchOrder;
