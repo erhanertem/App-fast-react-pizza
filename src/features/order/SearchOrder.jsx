df03fcdf701622eb69b06f6b230e47dfc36ef151
Erhan Ertem <eertem@gmail.com>
2024-09-21 18:59:56 +0300

    Style search box

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -1,18 +1,18 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function SearchOrder() {
-  const [query, setQuery] = useState('IIDSAT');
+  const [query, setQuery] = useState("IIDSAT");
 
   const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
     // GUARD CLAUSE - no entry nothing happens
-    if (query.trim() === '') return;
+    if (query.trim() === "") return;
 
     navigate(`/order/${query}`); //Navigate to the entered order#
-    setQuery(''); // Clear the input field after submission
+    setQuery(""); // Clear the input field after submission
   }
 
   return (
@@ -21,6 +21,7 @@ function SearchOrder() {
         placeholder="Search order #"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        className="w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50 sm:w-64 sm:focus:w-72"
       />
     </form>
   );
