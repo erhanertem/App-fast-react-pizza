d1ec25db87fb2c3ded0cef350501535c94bba978
Erhan Ertem <eertem@gmail.com>
2024-09-21 16:10:11 +0300

    Experiment with grids

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,22 +1,22 @@
-import Header from './Header';
-import CartOverview from './../features/cart/CartOverview';
-import { Outlet, useNavigation } from 'react-router-dom';
-import Loader from './Loader';
+import Header from "./Header";
+import CartOverview from "./../features/cart/CartOverview";
+import { Outlet, useNavigation } from "react-router-dom";
+import Loader from "./Loader";
 
 function AppLayout() {
   // THIS RR NAVIGATION STATE IS UNIVERSAL AND IS HELPFULL FOR LOADING STATE ANIMATIONS
   const navigation = useNavigation();
   // console.log(navigation);
   // NAVIGATIUON STATE IS EITHER IDLE|LOADING
-  const isLoading = navigation.state === 'loading';
+  const isLoading = navigation.state === "loading";
 
   return (
-    <div className="layout">
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
 
       <Header />
 
-      <main>
+      <main className="overflow-scroll">
         {/* Inserts component content of the corresponding endpoint element in the child routes definition @ router */}
         <Outlet />
       </main>
