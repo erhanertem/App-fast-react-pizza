f7834b6c8db99d9a88ed75a5d1c94315d11ad0dd
Erhan Ertem <eertem@gmail.com>
2024-09-21 18:35:30 +0300

    Experiment styling button elements' hover transition disabled states

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,30 +1,33 @@
 /* eslint-disable react-refresh/only-export-components */
 /* eslint-disable react/no-unescaped-entities */
-import { useState } from 'react';
-import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
-import { createOrder } from '../../services/apiRestaurant';
+import { useState } from "react";
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
+import { createOrder } from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
-const isValidPhone = (str) => /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(str);
+const isValidPhone = (str) =>
+  /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
+    str,
+  );
 
 const fakeCart = [
   {
     pizzaId: 12,
-    name: 'Mediterranean',
+    name: "Mediterranean",
     quantity: 2,
     unitPrice: 16,
     totalPrice: 32,
   },
   {
     pizzaId: 6,
-    name: 'Vegetale',
+    name: "Vegetale",
     quantity: 1,
     unitPrice: 13,
     totalPrice: 13,
   },
   {
     pizzaId: 11,
-    name: 'Spinach and Mushroom',
+    name: "Spinach and Mushroom",
     quantity: 1,
     unitPrice: 15,
     totalPrice: 15,
@@ -33,7 +36,7 @@ const fakeCart = [
 
 function CreateOrder() {
   const navigation = useNavigation();
-  const isSubmitting = navigation.state === 'submitting';
+  const isSubmitting = navigation.state === "submitting";
 
   // DISPLAY RETURNED DATA FROM THE ACTION FUNCTION - TYPICAL FOR FORM FIELD ERRORS
   const formErrors = useActionData();
@@ -52,21 +55,13 @@ function CreateOrder() {
       >
         <div>
           <label>First Name</label>
-          <input
-            type="text"
-            name="customer"
-            required
-          />
+          <input type="text" name="customer" required />
         </div>
 
         <div>
           <label>Phone number</label>
           <div>
-            <input
-              type="tel"
-              name="phone"
-              required
-            />
+            <input type="tel" name="phone" required />
           </div>
           {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
@@ -74,11 +69,7 @@ function CreateOrder() {
         <div>
           <label>Address</label>
           <div>
-            <input
-              type="text"
-              name="address"
-              required
-            />
+            <input type="text" name="address" required />
           </div>
         </div>
 
@@ -94,12 +85,13 @@ function CreateOrder() {
         </div>
 
         <div>
-          <input
-            type="hidden"
-            name="cart"
-            value={JSON.stringify(cart)}
-          />
-          <button disabled={isSubmitting}>{isSubmitting ? 'Placing order...' : 'Order now'}</button>
+          <input type="hidden" name="cart" value={JSON.stringify(cart)} />
+          <button
+            className="inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-1 active:bg-slate-400 disabled:cursor-not-allowed disabled:bg-slate-400"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? "Placing order..." : "Order now"}
+          </button>
         </div>
       </Form>
     </div>
@@ -139,25 +131,30 @@ export async function action({ request }) {
   // console.log(data);
 
   // POST PROCESS RETRIEVED ORDER DATA - MAKE USABLE BY OUR APP
-  const order = { ...data, cart: JSON.parse(data.cart), priority: data.priority === 'on' ? true : false };
+  const order = {
+    ...data,
+    cart: JSON.parse(data.cart),
+    priority: data.priority === "on" ? true : false,
+  };
   // console.log(order);
 
   // GUARD CLAUSE - check for required fields consistency
   const errors = {};
   if (!isValidPhone(order.phone)) {
-    errors.phone = 'Please give us your correct phone number. We might need it to contact you.';
+    errors.phone =
+      "Please give us your correct phone number. We might need it to contact you.";
   }
   if (Object.keys(errors).length > 0) {
     return errors;
   }
 
-  const newOrder = await createOrder(order);
+  // const newOrder = await createOrder(order);
   // console.log(newOrder);
 
   // ALWAYS RR LOADER OR ACTION FUNCTIONS REQUIRE A RETURN EVENTHOUGH WE DONT NEED IT
-  // return null;
+  return null;
   // WE CANT USE USENAVIGATE IN A FUNCTION TO PROGRAMMATICALLY NAVIGATE TO AN ENDPOINT, THEY ARE FOR REACT COMPONENTS. HOWEVER FOR CASUAL FUNCTIONS RR PROVIDES REDIRECT FUNCTION AS A PAR SUBSTITUTE
-  return redirect(`/order/${newOrder.id}`);
+  // return redirect(`/order/${newOrder.id}`);
 }
 
 export default CreateOrder;
