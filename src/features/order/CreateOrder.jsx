4caa670a3943e745318be74dd06146ee86631e93
Erhan Ertem <eertem@gmail.com>
2024-09-21 19:08:05 +0300

    Style a checkbox

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -80,6 +80,7 @@ function CreateOrder() {
 
         <div>
           <input
+            className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring-1 focus:ring-yellow-400 focus:ring-offset-2"
             type="checkbox"
             name="priority"
             id="priority"
