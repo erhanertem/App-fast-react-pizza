e6460eb7d389690cdca20a6db8d39ebd941796ae
Erhan Ertem <eertem@gmail.com>
2024-09-23 10:58:57 +0300

    Customize an existing tailwind configuration property with the new entry

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,16 @@ export default {
         // NON-DESTRUCTIVE CUSTOM COLOR ADDITION
         pizza: "#123456",
       },
+      height: {
+        // NON-DESTRUCTIVE OVERRIDE EXISTING PROPERTY
+        /*
+        dvh (Dynamic Viewport Height)
+        Definition: 1dvh is equal to 1% of the height of the dynamic viewport.
+        Behavior: Unlike vh, dvh adjusts when the viewport size changes dynamically. This is particularly useful for mobile devices where the height of the viewport may change when browser toolbars shrink, or when virtual keyboards appear/disappear. dvh recalculates based on the visible part of the viewport at any given time, making it more reliable for responsive designs, especially on mobile.
+        Use Case: dvh is a better choice when you need dynamic responsiveness to changes in the viewport, such as resizing, mobile keyboards, or shrinking browser toolbars.
+        */
+        screen: "100dvh",
+      },
     },
   },
   plugins: [],
