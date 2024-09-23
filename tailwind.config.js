7ac1626c6f179ba011ad97193633028aff6d5fac
Erhan Ertem <eertem@gmail.com>
2024-09-23 10:47:41 +0300

    Customize tailwind configuration - add custom color / add custom font for sans

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    theme: {
+      // fontFamily: {
+      //   // DESTRUCTIVE FONT FAMILY ADDITION
+      //   pizza: "Roboto Mono, monospace",
+      // },
+    },
+    extend: {
+      fontFamily: {
+        // NON-DESTRUCTIVE FONT FAMILY ADDITION
+        sans: "Roboto Mono, monospace",
+      },
+      colors: {
+        // NON-DESTRUCTIVE CUSTOM COLOR ADDITION
+        pizza: "#123456",
+      },
+    },
   },
   plugins: [],
 };
