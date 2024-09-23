13ad92f292ccdd4d68324bf57d6f67980b86e0cb
Erhan Ertem <eertem@gmail.com>
2024-09-23 23:07:19 +0300

    Establish cartSlice and register to RTK store

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,57 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cart: [
+    {
+      pizzaId: 12,
+      name: "Mediterranean",
+      quantity: 2,
+      unitPrice: 16,
+      totalPrice: 32,
+    },
+  ],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem(state, action) {
+      // Payload -> newItem
+      state.cart.push(action.payload);
+    },
+    deleteItem(state, action) {
+      // Payload -> pizzaId
+      state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
+    },
+    increaseItemQuantity(state, action) {
+      // Payload -> pizzaId
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      if (item) {
+        item.quantity++;
+        item.totalPrice = item.quantity * item.unitPrice;
+      }
+    },
+    decreaseItemQuantity(state, action) {
+      // Payload -> pizzaId
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      if (item) {
+        item.quantity--;
+        item.totalPrice = item.quantity * item.unitPrice;
+      }
+    },
+    clearCart(state) {
+      state.cart = [];
+    },
+  },
+});
+
+export const {
+  addItem,
+  deleteItem,
+  increaseItemQuantity,
+  decreaseItemQuantity,
+  clearCart,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
--- src/store.js
+++ src/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 
 // #1. IMPORT SLICE REDUCER
 import userReducer from "./features/user/userSlice";
+import cartReducer from "./features/cart/cartSlice";
 
 // #2. CONFIGURE THE REDUX STORE FOR MULTI ROOT SETUP
 const store = configureStore({
   reducer: {
     user: userReducer,
+    cart: cartReducer,
   },
 });
 
