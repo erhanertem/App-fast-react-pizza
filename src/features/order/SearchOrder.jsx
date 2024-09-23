519b7af9152229b87ae00cd3e27452175b4ecd60
Erhan Ertem <eertem@gmail.com>
2024-09-23 22:34:06 +0300

    Fix search order temp initial state

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function SearchOrder() {
-  const [query, setQuery] = useState("IIDSAT");
+  const [query, setQuery] = useState("");
 
   const navigate = useNavigate();
 
