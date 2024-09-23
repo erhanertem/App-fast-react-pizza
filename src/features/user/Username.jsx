ff73000461ea59892c7f92faa0a7d2fc79c7d663
Erhan Ertem <eertem@gmail.com>
2024-09-23 22:13:40 +0300

    Consume RTK user slicer state

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,15 @@
+import { useSelector } from "react-redux";
+
 function Username() {
-  return <div className="hidden text-sm font-semibold md:block">Erhan</div>;
+  // Read state from RTK store
+  const username = useSelector((state) => state.user.username);
+
+  // GUARD CLAUSE
+  if (!username) return null;
+
+  return (
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
+  );
 }
 
 export default Username;
