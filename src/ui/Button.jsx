450a60b6c96fa583e82f80520d69d5a9da8f81aa
Erhan Ertem <eertem@gmail.com>
2024-09-25 21:29:43 +0300

    Fix button css to respond to more breakpoints

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -4,7 +4,7 @@ function Button({ children, disabled = false, to, type, onClick }) {
   const styles = {
     base: "inline-block text-sm rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-1 active:bg-slate-400 disabled:cursor-not-allowed disabled:bg-slate-400",
     primary: "px-4 py-3 sm:px-6 sm:py-4",
-    small: "px-4 py-2 md:px-5 md:py-2.5 text-xs",
+    small: "px-4 py-2 md:px-5 md:py-2.5 text-xs p-4",
     secondary:
       "px-4 py-2.5 sm:px-6 sm:py-3.5 inline-block rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:text-stone-800 focus:ring-offset-1 disabled:cursor-not-allowed disabled:bg-slate-400",
     round: "px-2.5 py-1 md:px-3.5 md:p-2 text-sm",
