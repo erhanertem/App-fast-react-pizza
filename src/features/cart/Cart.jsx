450b7746679472bef8c077c8892d6d521de85c6f
Erhan Ertem <eertem@gmail.com>
2024-09-25 21:21:47 +0300

    Update the useSelector function from the userSlicer @ cart.jsx

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -2,38 +2,14 @@ import LinkButton from "../../ui/LinkButton";
 import Button from "../../ui/Button";
 import CartItem from "./CartItem";
 import { useDispatch, useSelector } from "react-redux";
-import { clearCart, getCart, getUsername } from "./cartSlice";
+import { clearCart, getCart } from "./cartSlice";
 import EmptyCart from "./EmptyCart";
-
-// const fakeCart = [
-//   {
-//     pizzaId: 12,
-//     name: "Mediterranean",
-//     quantity: 2,
-//     unitPrice: 16,
-//     totalPrice: 32,
-//   },
-//   {
-//     pizzaId: 6,
-//     name: "Vegetale",
-//     quantity: 1,
-//     unitPrice: 13,
-//     totalPrice: 13,
-//   },
-//   {
-//     pizzaId: 11,
-//     name: "Spinach and Mushroom",
-//     quantity: 1,
-//     unitPrice: 15,
-//     totalPrice: 15,
-//   },
-// ];
+import { getUser } from "../user/userSlice";
 
 function Cart() {
   // READ username FROM RTK STORE
-  const username = useSelector(getUsername);
+  const { username } = useSelector(getUser);
   const cart = useSelector(getCart);
-  // const cart = fakeCart;
 
   // Instantiate a action fn dispatcher to update RTK state
   const dispatch = useDispatch();
